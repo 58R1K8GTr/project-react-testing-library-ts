aa3eb8f95161f4b97dd3fa2c3248e8da375bd4ea
b166erobot <bernard0freitas@outlook.com>
2024-04-28 13:45:53 -0300

    Requisito 4 satisfeito

--- src/tests/NotFound.test.tsx
+++ src/tests/NotFound.test.tsx
@@ -1,1 +1,18 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+beforeEach(() => {
+  renderWithRouter(<App />, { route: '/digimon' });
+});
+
+describe('testando o componente NotFound', () => {
+  test('testando se a página contém um h2 com um certo texto', () => {
+    const h2NotFound = screen.getByRole('heading', { name: /page requested not found/i });
+    expect(h2NotFound).toBeVisible();
+  });
+  test('testando se a página mostra a imagem com o texto alternativo', () => {
+    const img = screen.getByRole('img', { name: /Clefairy pushing buttons randomly with text I have no idea what i'm doing/ });
+    expect(img).toBeVisible();
+  });
+});
