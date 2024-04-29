e96b23180356ef160df555295f0b7ffbfd3c1698
b166erobot <bernard0freitas@outlook.com>
2024-04-29 20:23:39 -0300

    Requisito 5 satisfeito, até que enfim entendi o que era pra fazer

--- src/tests/Pokedex.test.tsx
+++ src/tests/Pokedex.test.tsx
@@ -3,18 +3,9 @@ import App from '../App';
 import renderWithRouter from '../renderWithRouter';
 import pokemonList from '../data';
 
-const pokemonListNames = [
-  'Charmander',
-  'Caterpie',
-  'Ekans',
-  'Alakazam',
-  'Mew',
-  'Rapidash',
-  'Snorlax',
-  'Dragonair',
-];
-
+const pokemonListNames = pokemonList.map((pokemon) => pokemon.name);
 const buttonNames = ['Electric', 'Fire', 'Bug', 'Poison', 'Psychic', 'Normal', 'Dragon'];
+const testIdPokemonName = 'pokemon-name';
 
 describe('testando o componente pokedex', () => {
   test('testando se a página tem um h2 com um certo texto', () => {
@@ -22,27 +13,59 @@ describe('testando o componente pokedex', () => {
     const h2 = screen.getByRole('heading', { name: /encountered pokémon/i });
     expect(h2).toBeVisible();
   });
-  test('testando se é exibido o próximo pokemon da lista', () => {
+  test('testando se é exibido o próximo pokemon da lista', async () => {
     const { user } = renderWithRouter(<App />);
-    pokemonListNames.forEach(async (pokemon) => {
-      const nextPokemonButton = screen.getByRole('button', { name: /próximo pokémon/i });
-      expect(nextPokemonButton).toHaveTextContent(/próximo pokémon/i);
-      expect(nextPokemonButton).toBeVisible();
-      await user.click(nextPokemonButton);
-      const pokemonName = screen.getByText(pokemon);
-      expect(pokemonName).toHaveTextContent(pokemon);
-      expect(pokemonName).toBeVisible();
-    });
-    const pokemonName = screen.getByText('Pikachu');
-    expect(pokemonName).toHaveTextContent('Pikachu');
-    expect(pokemonName).toBeVisible();
+    const nextPokemonButton = screen.getByRole('button', { name: /próximo pokémon/i });
+    expect(nextPokemonButton).toBeVisible();
+    // amaldiçoado seja o javascript que não permite um forEach nos testes.
+    await user.click(nextPokemonButton);
+    let renderedPokemon = screen.getByTestId(testIdPokemonName);
+    expect(renderedPokemon).toHaveTextContent('Charmander');
+    expect(renderedPokemon).toBeVisible();
+    await user.click(nextPokemonButton);
+    renderedPokemon = screen.getByTestId(testIdPokemonName);
+    expect(renderedPokemon).toHaveTextContent('Caterpie');
+    expect(renderedPokemon).toBeVisible();
+    await user.click(nextPokemonButton);
+    renderedPokemon = screen.getByTestId(testIdPokemonName);
+    expect(pokemonListNames).toContain(renderedPokemon.textContent);
+    expect(renderedPokemon).toBeVisible();
+    await user.click(nextPokemonButton);
+    renderedPokemon = screen.getByTestId(testIdPokemonName);
+    expect(pokemonListNames).toContain(renderedPokemon.textContent);
+    expect(renderedPokemon).toBeVisible();
+    await user.click(nextPokemonButton);
+    renderedPokemon = screen.getByTestId(testIdPokemonName);
+    expect(pokemonListNames).toContain(renderedPokemon.textContent);
+    expect(renderedPokemon).toBeVisible();
+    await user.click(nextPokemonButton);
+    renderedPokemon = screen.getByTestId(testIdPokemonName);
+    expect(pokemonListNames).toContain(renderedPokemon.textContent);
+    expect(renderedPokemon).toBeVisible();
+    await user.click(nextPokemonButton);
+    renderedPokemon = screen.getByTestId(testIdPokemonName);
+    expect(pokemonListNames).toContain(renderedPokemon.textContent);
+    expect(renderedPokemon).toBeVisible();
+    await user.click(nextPokemonButton);
+    renderedPokemon = screen.getByTestId(testIdPokemonName);
+    expect(pokemonListNames).toContain(renderedPokemon.textContent);
+    expect(renderedPokemon).toBeVisible();
+    await user.click(nextPokemonButton);
+    renderedPokemon = screen.getByTestId(testIdPokemonName);
+    expect(pokemonListNames).toContain(renderedPokemon.textContent);
+    expect(renderedPokemon).toBeVisible();
   });
   test('testando se é mostrado apenas um pokémon por vez', () => {
     const { user } = renderWithRouter(<App />);
+    const nextPokemonButton = screen.getByRole('button', { name: /próximo pokémon/i });
+
+    async function click() {
+      user.click(nextPokemonButton);
+    }
+
     pokemonListNames.forEach(async () => {
-      const nextPokemonButton = screen.getByRole('button', { name: /próximo pokémon/i });
-      await user.click(nextPokemonButton);
-      const pokemon = screen.getByTestId('pokemon-name');
+      await click();
+      const pokemon = screen.getByTestId(testIdPokemonName);
       expect(pokemon).toBeVisible();
     });
   });
@@ -54,29 +77,40 @@ describe('testando o componente pokedex', () => {
     });
     expect(buttons).length(7);
   });
-  test('testando se ao selecionar um tipo, a pokédex circula somente com os pokémons daquele tipo', () => {
+  test('testando se ao selecionar um tipo, a pokédex circula somente com os pokémons daquele tipo', async () => {
     const { user } = renderWithRouter(<App />);
-    const pokemonsFilteredByType = pokemonList
-      .reduce<{ [key: string]: string[] }>((acc, item) => {
-      if (!acc[item.type]) {
-        acc[item.type] = [];
-      }
-      acc[item.type].push(item.name);
-      return acc;
-    }, {});
-    Object.keys(pokemonsFilteredByType).forEach(async (typePokemon: string) => {
-      const button = screen.getByRole('button', { name: typePokemon });
-      const buttonAll = screen.getByRole('button', { name: 'All' });
-      await user.click(button);
-      expect(buttonAll).toBeVisible();
-      pokemonsFilteredByType[typePokemon].forEach(async (pokemonName) => {
-        const pokemon = screen.getByTestId('pokemon-name');
-        expect(pokemon).toHaveTextContent(pokemonName);
-        if (pokemonsFilteredByType[typePokemon].length > 1) {
-          const nextPokemonButton = screen.getByRole('button', { name: /próximo pokémon/i });
-          await user.click(nextPokemonButton);
-        }
-      });
-    });
+    // código refatorado pois o outro não funcionava e eu fiquei dias travado nisso.
+    // fazer na mão ao invés de um forEach aqui sai mais fácil tanto pro código quanto pra mente que está cansada.
+    const buttonNext = screen.getByRole('button', { name: /próximo pokémon/i });
+    const buttonFilterFire = screen.getByRole('button', { name: 'Fire' });
+    const buttonFilterPsychic = screen.getByRole('button', { name: 'Psychic' });
+    const pokemonsPsychic = ['Alakazam', 'Mew'];
+    const pokemonsFire = ['Charmander', 'Rapidash'];
+    await user.click(buttonFilterPsychic);
+    let renderedPokemon = screen.getByTestId(testIdPokemonName);
+    expect(pokemonsPsychic).toContain(renderedPokemon.textContent);
+    await user.click(buttonNext);
+    renderedPokemon = screen.getByTestId(testIdPokemonName);
+    expect(pokemonsPsychic).toContain(renderedPokemon.textContent);
+    await user.click(buttonFilterFire);
+    renderedPokemon = screen.getByTestId(testIdPokemonName);
+    expect(pokemonsFire).toContain(renderedPokemon.textContent);
+    await user.click(buttonNext);
+    renderedPokemon = screen.getByTestId(testIdPokemonName);
+    expect(pokemonsFire).toContain(renderedPokemon.textContent);
+  });
+  test('testando se o botão All é clicável', async () => {
+    const { user } = renderWithRouter(<App />);
+    const buttonAll = screen.getByRole('button', { name: 'All' });
+    const buttonFilter = screen.getByRole('button', { name: 'Fire' });
+    let renderedPokemon = screen.getByTestId(testIdPokemonName);
+    const pokemonName = 'Pikachu';
+    expect(renderedPokemon).toHaveTextContent(pokemonName);
+    await user.click(buttonFilter);
+    renderedPokemon = screen.getByTestId(testIdPokemonName);
+    expect(renderedPokemon).not.toHaveTextContent(pokemonName);
+    await user.click(buttonAll);
+    renderedPokemon = screen.getByTestId(testIdPokemonName);
+    expect(renderedPokemon).toHaveTextContent(pokemonName);
   });
 });
